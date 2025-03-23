f3142ff76060e4e3d64317677646758b0b0d5aab
Rajendra Kumar <8026549+rjdkmr@users.noreply.github.com>
2025-03-23 22:57:45 +0530

    Update build_macos_wheels.sh

--- scripts/build_macos_wheels.sh
+++ scripts/build_macos_wheels.sh
@@ -7,47 +7,41 @@ brew install gcc@14 libomp gsl fftw pyenv eigen boost suite-sparse openblas cmak
 brew link --force openblas libomp
 brew cleanup
 
-eval "$(pyenv init -)"
-pyenv install --list
-PYVERS=("3.9" "3.10" "3.11" "3.12")
-PYTHONS=()
-for PYVER in ${PYVERS[@]}
-do
-    # Install the latest release of the specified Python version using pyenv.
-    PYVER="$(pyenv install --list | grep -E "^\\s*$PYVER" | sort -n -t. -k3 | tail -n1)"
-    pyenv install $PYVER
-    pyenv global $PYVER
-    PYTHONS+=($PYVER)
-    echo $PYVER
-    echo $(python --version)
-    python -m pip install --upgrade pip
-    python -m pip install -r ${CWD}/dev-requirements.txt
-    python -m pip install delocate
-done
-pyenv global system
-
-CWD=`pwd`
-
 cd external
 mkdir apbs_installed
 mkdir gmx_installed
+mkdir fetk_installed
 
 export CC=gcc-14
 export CXX=g++-14
-#export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/homebrew/opt/openblas/lib/pkgconfig"
-#export LDFLAGS="${LDFLAGS} -L/opt/homebrew/opt/libomp/lib -L/opt/homebrew/opt/openblas/lib -lm"
-#export CPPFLAGS="${CPPFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
-#export CFLAGS="${CFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
-#export CXXFLAGS="${CXXFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/homebrew/opt/openblas/lib/pkgconfig"
+export LDFLAGS="${LDFLAGS} -L/opt/homebrew/opt/libomp/lib -L/opt/homebrew/opt/openblas/lib -lm"
+export CPPFLAGS="${CPPFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
+export CFLAGS="${CFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
+export CXXFLAGS="${CXXFLAGS} -I/opt/homebrew/opt/libomp/include -I/opt/homebrew/opt/openblas/include"
 
-export APBS_INSTALL=${CWD}/external/apbs_installed
+wget https://github.com/Electrostatics/FETK/archive/refs/tags/1.9.3.tar.gz -O fetk.tar.gz
+tar -zxf fetk.tar.gz
+cd FETK-1.9.3
+mkdir build
+cd build
+cmake .. -DBLA_STATIC=OFF -DBUILD_SUPERLU=OFF -DCMAKE_INSTALL_PREFIX=${CWD}/external/fetk_installed
+make
+make install
+cd ${CWD}/external
 
 cd apbs/
-if [ -d build ]; then
-    rm -rf build
-fi
+sed "-i" "" "-e" "s/include\(ImportFETK\)//g" CMakeLists.txt
+sed "-i" "" "-e" "s/import_fetk\(\${FETK_VERSION}\)//g" CMakeLists.txt
+cat CMakeLists.txt
 
 mkdir build && cd build
+
+export APBS_INSTALL=${CWD}/external/apbs_installed
+export LDFLAGS="${LDFLAGS} -L${CWD}/external/fetk_installed/lib"
+export CPPFLAGS="${CPPFLAGS} -I${CWD}/external/fetk_installed/include"
+export CFLAGS="${CFLAGS} -I${CWD}/external/fetk_installed/include"
+export CXXFLAGS="${CXXFLAGS} -I${CWD}/external/fetk_installed/include"
 
 cmake .. \
   -DCMAKE_INSTALL_INCLUDEDIR="include" \
@@ -65,11 +59,12 @@ cmake .. \
   -DENABLE_PBSAM=OFF \
   -DENABLE_PYTHON=OFF \
   -DENABLE_TESTS=OFF \
-  -DFETK_VERSION=86e074ecfa19b818e1896ea44082d14bc7e44082 \
   -DGET_NanoShaper=OFF \
   -DCMAKE_FIND_FRAMEWORK=NEVER \
   -DCMAKE_FIND_APPBUNDLE=NEVER \
-  -DBLA_STATIC=OFF   \
+  -DCMAKE_MODULE_PATH=${CWD}/external/fetk_installed/share/fetk/cmake   \
+  -DAPBS_LIBS=mc;maloc \
+  -DHOMEBREW_ALLOW_FETCHCONTENT=OFF \
   -DCMAKE_VERBOSE_MAKEFILE=ON
 
 make 
@@ -92,6 +87,25 @@ make
 make install
 
 cd ${CWD}
+
+eval "$(pyenv init -)"
+pyenv install --list
+PYVERS=("3.9" "3.10" "3.11" "3.12")
+PYTHONS=()
+for PYVER in ${PYVERS[@]}
+do
+    # Install the latest release of the specified Python version using pyenv.
+    PYVER="$(pyenv install --list | grep -E "^\\s*$PYVER" | sort -n -t. -k3 | tail -n1)"
+    pyenv install $PYVER
+    pyenv global $PYVER
+    PYTHONS+=($PYVER)
+    echo $PYVER
+    echo $(python --version)
+    python -m pip install --upgrade pip
+    python -m pip install -r ${CWD}/dev-requirements.txt
+    python -m pip install delocate
+done
+pyenv global system
 
 mkdir wheels
 mkdir fixed_wheels
