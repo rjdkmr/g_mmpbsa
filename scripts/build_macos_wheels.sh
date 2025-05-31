2b803d561f4efe288a0119e4a1dc8da4fb72d529
Rajendra Kumar <rjdkmr@gmail.com>
2025-05-31 13:36:54 +0000

    fixing macos wheel build

--- scripts/build_macos_wheels.sh
+++ scripts/build_macos_wheels.sh
@@ -9,17 +9,37 @@ brew cleanup
 cd external
 mkdir gmx_installed
 mkdir apbs_installed
+mkdir fetk_installed
+
+wget https://github.com/Electrostatics/FETK/archive/refs/tags/1.9.3.tar.gz
+tar -xzf 1.9.3.tar.gz
+ls -lhrt
+cd FETK-1.9.3
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${CWD}/external/fetk_installed \
+    -DCMAKE_C_FLAGS="-Wno-error=implicit-int" \
+    -DBLA_STATIC=OFF \
+    -DBUILD_SUPERLU=OFF
+
+make || exit 1
+make install
+
+ls -lhrt ${CWD}/external/fetk_installed/
+ls -lhrt ${CWD}/external/fetk_installed/share
+
+cd $CWD/external
 
 cd apbs/
 if [ -d build ]; then
     rm -rf build
 fi
 
+sed -i -e 's/include\(ImportFETK\)//g' CMakeLists.txt
+sed -i -e 's/import_fetk\(\$\{FETK_VERSION\}\)//g' CMakeLists.txt
+cat CMakeLists.txt
+
 mkdir build && cd build
-export BUILD_SUPERLU=OFF
-export BLA_STATIC=OFF
-export LDFLAGS="-L/opt/homebrew/opt/lapack/lib:-L/opt/homebrew/opt/openblas/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/lapack/include:-I/opt/homebrew/opt/openblas/include"
 
 cmake .. \
   -DCMAKE_INSTALL_INCLUDEDIR="include" \
@@ -39,10 +59,11 @@ cmake .. \
   -DENABLE_TESTS=OFF \
   -DFETK_VERSION=57195e55351e04ce6ee0ef56a143c996a9aee7e2 \
   -DGET_NanoShaper=OFF \
-  -DCMAKE_C_FLAGS="-fpermissive" \
+  -DCMAKE_C_FLAGS="-fpermissive -I${CWD}/external/fetk_installed/include -Wno-error=incompatible-pointer-types" \
   -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-  -DBLA_STATIC=OFF \
-  -DBUILD_SUPERLU=OFF \
+  -DAPBS_LIBS="mc;maloc" \
+  -DCMAKE_MODULE_PATH=${CWD}/external/fetk_installed/share/fetk/cmake \
+  -DCMAKE_EXE_LINKER_FLAGS=-L${CWD}/external/fetk_installed/lib
 
 make || exit 1
 make install
