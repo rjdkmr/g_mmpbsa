d3a0c27a75e095407fb2cae08825c4e4878010aa
Rajendra Kumar <rjdkmr@gmail.com>
2025-06-01 12:27:49 +0000

    fixing macos test

--- scripts/test_macos_wheels.sh
+++ scripts/test_macos_wheels.sh
@@ -3,14 +3,15 @@ set -e -x
 
 CWD=`pwd`
 
+brew install pipx
 brew install brewsci/bio/apbs
 brew cleanup
 export APBS=$(brew --prefix apbs)/bin/apbs
 
 cd test
 
-python3 -m pip install g-mmpbsa
-python3 -c "import g_mmpbsa; print('=====\nTEST -- g_mmpbsa GROMACS version: ', g_mmpbsa.gmx_version, '\n=====')"
+pipx ensurepath
+pipx install g-mmpbsa
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -pdie 2 -decomp -unit1 Protein -unit2 BEC
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i polar_orig/mmpbsa.mdp -e 1000 -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -pol polar.xvg -pcon contrib_pol.dat
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i sasa_orig/mmpbsa.mdp -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -apol sasa.xvg -apcon sasa_contrib.dat
