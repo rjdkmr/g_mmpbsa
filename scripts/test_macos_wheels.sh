3f729f243166890f922bfd787829073d8ef79de1
Rajendra Kumar <rjdkmr@gmail.com>
2025-06-01 12:36:12 +0000

    testing macos wheels

--- scripts/test_macos_wheels.sh
+++ scripts/test_macos_wheels.sh
@@ -10,8 +10,7 @@ export APBS=$(brew --prefix apbs)/bin/apbs
 
 cd test
 
-pipx ensurepath
-pipx install g-mmpbsa
+pipx install g-mmpbsa==3.0.7
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -pdie 2 -decomp -unit1 Protein -unit2 BEC
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i polar_orig/mmpbsa.mdp -e 1000 -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -pol polar.xvg -pcon contrib_pol.dat
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i sasa_orig/mmpbsa.mdp -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -apol sasa.xvg -apcon sasa_contrib.dat
