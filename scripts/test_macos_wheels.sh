bd6ed358bf9945f62b8f44b29d8cb347ea793f79
Rajendra Kumar <rjdkmr@gmail.com>
2025-06-01 15:02:10 +0000

    change gh workflow on tagged only

--- scripts/test_macos_wheels.sh
+++ scripts/test_macos_wheels.sh
@@ -13,7 +13,7 @@ which pipx
 pipx install g-mmpbsa
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -pdie 2 -decomp -unit1 Protein -unit2 BEC
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i polar_orig/mmpbsa.mdp -e 1000 -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -pol polar.xvg -pcon contrib_pol.dat
-g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i sasa_orig/mmpbsa.mdp -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -apol sasa.xvg -apcon sasa_contrib.dat
+g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i sasa_orig/mmpbsa.mdp -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -apol sasa.xvg -apcon sasa_contrib.dat -silent
 cat polar.xvg
 cat sasa.xvg
 cat energy_MM.xvg
