b7610d1492b0dc5a1efc75e17161386f86e06d7d
Rajendra Kumar <rjdkmr@gmail.com>
2026-08-01 09:59:47 +0000

    fixed macos test wheel

--- scripts/test_macos_wheels.sh
+++ scripts/test_macos_wheels.sh
@@ -7,10 +7,11 @@ brew install brewsci/bio/apbs
 brew cleanup
 export APBS=$(brew --prefix apbs)/bin/apbs
 
+python -m pip install --upgrade pip
+
 cd test
 
-which pipx
-pipx install g-mmpbsa
+python -m pip install --upgrade g_mmpbsa
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -pdie 2 -decomp -unit1 Protein -unit2 BEC
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i polar_orig/mmpbsa.mdp -e 1000 -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -pol polar.xvg -pcon contrib_pol.dat
 g_mmpbsa run -f data/1EBZ.xtc -s data/1EBZ.tpr -n data/1EBZ.ndx -i sasa_orig/mmpbsa.mdp -nomme -pbsa -decomp -unit1 Protein -unit2 BEC -apol sasa.xvg -apcon sasa_contrib.dat -silent
