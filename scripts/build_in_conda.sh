dce548a39fa83fcabe991019be8b5147e743853a
Rajendra Kumar <rjdkmr@gmail.com>
2026-08-01 07:52:00 +0000

    updated for gromacs-2026.3

--- scripts/build_in_conda.sh
+++ scripts/build_in_conda.sh
@@ -59,11 +59,11 @@ if [ -d build ]; then
 fi
 mkdir build && cd build
 
-export GMX_PATH=${CWD}/external/gmx_installed
+export GMX_INSTALL=${CWD}/external/gmx_installed
 export GMX_SRC=${CWD}/external/gromacs
 
 cmake .. -DGMX_SIMD=NONE -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on \
-             -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_PATH}
+             -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL}
             
 make
 make install
